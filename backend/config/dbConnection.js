2a8da91582944d2a687c70addb5d30ec3e2a36da
Ishuuutanejaaa <ishutaneja3110@gmail.com>
2024-11-19 10:40:29 +0530

    LoginFinal

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
-const connectDb=async()=>{
-    try{
-        const connect=await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("Database connected: ",
-            connect.connection.host,
-            connect.connection.name
-        );
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit with failure
+  }
 };
+
 module.exports = connectDb;
--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,53 +1,126 @@
+// const asyncHandler = require("express-async-handler");
+// const bcrypt = require('bcrypt');
+// const jwt = require("jsonwebtoken");
+// const Person = require("../model/loginModel");
+// require("dotenv").config();
+
+// const registerPerson = asyncHandler(async(req,res)=>{
+//     const{email , name , gender , address ,phoneNumber,  password} = req.body;
+//     if(!email || !name || !gender || !address || !phoneNumber || !password){
+//         res.status(400);
+//         throw new Error("Please provide all fields");
+//     }
+
+//     const personExists = await Person.findOne({email});
+//     if (personExists){
+//         return res.status(400).json({message: "User already exists"});
+//     }
+
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password , salt);
+
+//     const person = await Person.create({
+//         email,
+//         name,
+//         gender,
+//         address,
+//         phoneNumber,
+//         password : hashedPassword,
+//     });
+
+//     const token = jwt.sign({id: person._id}, process.env.JWT_SECRET , {expiresIn :"1d"});
+//     res.status(201).json({message:"User registered successfully.",person,token});
+// });
+
+// const loginPerson = asyncHandler(async(req,res)=>{
+//     const {email,password}=req.body;
+//     if(!email || !password){
+//         res.status(400);
+//         throw new Error("Please provide all the fields.");
+//     }
+//     const person = await Person.findOne({email});
+//     if(!person){
+//         return res.status(400).json({message:"User not found Please register first"});
+//     }
+//     const isMatch = await bcrypt.compare(password, person.password);
+//     if(!isMatch){
+//         return res.status(400).json({message:"Invalid Password"});
+//     }
+//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+//     res.status(200).json({message:"Login succesful and you are being directed to home page" ,token ,  person: {email: person.email , name: person.name}});
+// });
+
+// module.exports = {registerPerson , loginPerson};
+
 const asyncHandler = require("express-async-handler");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const Person = require("../model/loginModel");
 require("dotenv").config();
 
-const registerPerson = asyncHandler(async(req,res)=>{
-    const{email , name , gender , address ,phoneNumber,  password} = req.body;
-    if(!email || !name || !gender || !address || !phoneNumber || !password){
-        res.status(400);
-        throw new Error("Please provide all fields");
-    }
+// Register Person
+const registerPerson = asyncHandler(async (req, res) => {
+  const { email, name, gender, address, phoneNumber, password } = req.body;
 
-    const personExists = await Person.findOne({email});
-    if (personExists){
-        return res.status(400).json({message: "User already exists"});
-    }
+  if (!email || !name || !gender || !address || !phoneNumber || !password) {
+    res.status(400);
+    throw new Error("Please provide all fields");
+  }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password , salt);
+  // Check if user already exists
+  const personExists = await Person.findOne({ email });
+  if (personExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
 
-    const person = await Person.create({
-        email,
-        name,
-        gender,
-        address,
-        phoneNumber,
-        password : hashedPassword,
-    });
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    const token = jwt.sign({id: person._id}, process.env.JWT_SECRET , {expiresIn :"1d"});
-    res.status(201).json({message:"User registered successfully.",person,token});
+  // Create a new person
+  const person = await Person.create({
+    email,
+    name,
+    gender,
+    address,
+    phoneNumber,
+    password: hashedPassword,
+  });
+
+  if (person) {
+    res.status(201).json({ message: "User registered successfully" });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
-const loginPerson = asyncHandler(async(req,res)=>{
-    const {email,password}=req.body;
-    if(!email || !password){
-        res.status(400);
-        throw new Error("Please provide all the fields.");
-    }
-    const person = await Person.findOne({email});
-    if(!person){
-        return res.status(400).json({message:"User not found Please register first"});
-    }
-    const isMatch = await bcrypt.compare(password, person.password);
-    if(!isMatch){
-        return res.status(400).json({message:"Invalid Password"});
-    }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res.status(200).json({message:"Login succesful and you are being directed to home page" ,token ,  person: {email: person.email , name: person.name}});
+// Login Person
+const loginPerson = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please provide all fields.");
+  }
+
+  const person = await Person.findOne({ email });
+  if (!person) {
+    return res.status(400).json({ message: "User not found. Please register first." });
+  }
+
+  const isMatch = await bcrypt.compare(password, person.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid password" });
+  }
+
+  const token = jwt.sign({ id: person._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+  res.status(200).json({
+    message: "Login successful, redirecting to home page",
+    token,
+    person: { email: person.email, name: person.name },
+  });
 });
 
-module.exports = {registerPerson , loginPerson};
+module.exports = { registerPerson, loginPerson };

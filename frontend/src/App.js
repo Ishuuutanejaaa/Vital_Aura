95c07fbe6a6496e2c2fa7d22a33ac9a83e5bdebe
Ishuuutanejaaa <ishutaneja3110@gmail.com>
2024-11-19 10:55:04 +0530

    loginfinal2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import Login from './Components/Login'; // Corrected to reference the Login.jsx file
-import HomePage from './Components/HomePage/HomePage';
+import Login from './Components/Login'; // Corrected to reference the Login.jsx file
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                 
+                <Route path="/" element={<Login />} /> {/* Ensure path matches the initial route */}
             </Routes>
         </Router>
     );
